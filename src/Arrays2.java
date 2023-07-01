cc15a38e0e86fca6e3ec5259fb62fc440bb1c076
Vincent <vincentallan001@gmail.com>
2023-07-01 02:31:13 -0700

    Fixed a bug

--- src/Arrays2.java
+++ src/Arrays2.java
@@ -4,11 +4,11 @@ public class Arrays2 {
     public int[] twoSum(int[] num, int target){
         for (int i = 0; i< num.length-1; i++){
             if (num[i] + num[i+1] == target){
-                int[] output = {i,i+1};
+                int[] output = new int[]{i,i+1};
                 System.out.println(Arrays.toString(output));
             }
         }
-        return num;
+        return new int[]{};
     }
 
     public static void main(String []args){
